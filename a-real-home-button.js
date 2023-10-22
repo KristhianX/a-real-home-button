a767977394b3fbff348b28d45046902b0fe80efd
Kristhian Bautista <46209998+KristhianX@users.noreply.github.com>
2023-10-21 21:01:36 -0500

    Update a-real-home-button.js

--- a-real-home-button.js
+++ a-real-home-button.js
@@ -4,7 +4,7 @@
 // @match            *://*/*
 // @noframes
 // @grant            window.close
-// @version          1.4.2
+// @version          1.4.3
 // @author           KristhianX
 // @description      Adds homepage, new tab and close tab buttons to android Firefox. 10/12/2023, 9:24:24 PM
 // @icon             data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" width="24px" height="24px" viewBox="0 0 24 24" fill="none" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-home"><path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path><polyline points="9 22 9 12 15 12 15 22"></polyline></svg>
@@ -14,8 +14,15 @@
 // ==/UserScript==
 
 
+// URL for the homepage and new tab buttons.
+// Set the height of the toolbar.
+const homepageURL = 'https://web.tabliss.io';
+const newTabURL = 'https://web.tabliss.io';
+const toolbarHeight = '7';
+
+
 // Inline svg icons and default css style for the buttons.
-// All Icons from https://github.com/feathericons/feather
+// All icons from https://github.com/feathericons/feather
 const svgHome = '<svg xmlns="http://www.w3.org/2000/svg" width="60%" height="60%" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" background="transparent" color="#fff" class="feather feather-home"><path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path><polyline points="9 22 9 12 15 12 15 22"></polyline></svg>';
 const svgPlus = '<svg xmlns="http://www.w3.org/2000/svg" width="60%" height="60%" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" background="transparent" color="#fff" class="feather feather-plus-square"><rect x="3" y="3" width="18" height="18" rx="2" ry="2"></rect><line x1="12" y1="8" x2="12" y2="16"></line><line x1="8" y1="12" x2="16" y2="12"></line></svg>';
 const svgUp = '<svg xmlns="http://www.w3.org/2000/svg" width="60%" height="60%" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" background="transparent" color="#fff" class="feather feather-arrow-up-circle"><circle cx="12" cy="12" r="10"></circle><polyline points="16 12 12 8 8 12"></polyline><line x1="12" y1="16" x2="12" y2="8"></line></svg>';
@@ -23,12 +30,6 @@ const svgDown = '<svg xmlns="http://www.w3.org/2000/svg" width="60%" height="60%
 const svgEyeOff = '<svg xmlns="http://www.w3.org/2000/svg" width="60%" height="60%" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" background="transparent" color="#fff" class="feather feather-eye-off"><path d="M17.94 17.94A10.07 10.07 0 0 1 12 20c-7 0-11-8-11-8a18.45 18.45 0 0 1 5.06-5.94M9.9 4.24A9.12 9.12 0 0 1 12 4c7 0 11 8 11 8a18.5 18.5 0 0 1-2.16 3.19m-6.72-1.07a3 3 0 1 1-4.24-4.24"></path><line x1="1" y1="1" x2="23" y2="23"></line></svg>';
 const svgClose = '<svg xmlns="http://www.w3.org/2000/svg" width="60%" height="60%" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" background="transparent" color="#fff" class="feather feather-x-circle"><circle cx="12" cy="12" r="10"></circle><line x1="15" y1="9" x2="9" y2="15"></line><line x1="9" y1="9" x2="15" y2="15"></line></svg>';
 const defaultButtonStyle = 'height: 100%; aspect-ratio: 1; cursor: pointer; border: none; border-radius: 20%; background: transparent';
-
-
-// URL for the homepage and new tab buttons.
-const homepageURL = 'https://web.tabliss.io';
-const newTabURL = 'https://web.tabliss.io';
-const toolbarHeight = '7';
 
 
 // Creating the iframe with the maximum z-index value to ensure it is allways on top.
